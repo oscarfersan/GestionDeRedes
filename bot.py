267db92e6f481f5f9c0a17c8126b6825bbff43e8
Oscar <oscar7fernandez@outlook.com>
2022-01-20 18:33:00 +0100

    Commit Final antes de entrega

--- bot.py
+++ bot.py
@@ -82,36 +82,27 @@ async def GR(ctx,*args):
 	client = MongoClient('localhost',27017)
 	db = client.grs
 	find = db.metric.find().sort('created_at', ASCENDING).limit(15)
-
 	if order == "Memory":
 		xList = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
 		yList = []#Obtener los 15 valores de memoria
 		for i in find:
 			yList.append(i['mem'])
-		
 		x = np.array(xList)
 		y = np.array(yList)
 		plt.plot(x,y)
-		##plt.title(f'{order}'s Graph')
 		plt.savefig(fname='plot')
 		await ctx.send(file = discord.File('plot.png'))
 		os.remove('plot.png')
 		plt.clf()
-
 	elif order == "CPU":
-
 		xList = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
 		yList = []#Obtener los 15 valores de memoria
 		for i in find:
 			yList.append(i['cpu'])
-
 		x = np.array(xList)
 		y = np.array(yList)
-		
 		plt.plot(x,y)
-		##plt.title(f'{order}'s Graph')
 		plt.savefig(fname='plot')
-
 		await ctx.send(file = discord.File('plot.png'))
 		os.remove('plot.png')
 		plt.clf() 
@@ -121,7 +112,7 @@ def BD ():
 	client = MongoClient('localhost',27017)
 	db = client.grs
 	while(True):
-		time.sleep(5)
+		time.sleep(30)
 		memoria = snmp.get('127.0.0.1',['1.3.6.1.4.1.2021.4.11.0'], hlapi.UsmUserData(SNMP_USER, authKey=SHA_PASSWORD, privKey=AES_PASSWORD, authProtocol=hlapi.usmHMACSHAAuthProtocol, privProtocol=hlapi.usmAesCfb128Protocol))['1.3.6.1.4.1.2021.4.11.0']
 		cpu = snmp.get('127.0.0.1',['1.3.6.1.4.1.2021.10.1.3.3'], hlapi.UsmUserData(SNMP_USER, authKey=SHA_PASSWORD, privKey=AES_PASSWORD, authProtocol=hlapi.usmHMACSHAAuthProtocol, privProtocol=hlapi.usmAesCfb128Protocol))['1.3.6.1.4.1.2021.10.1.3.3']
 		date = datetime.now()
